ccac45b9b793385405d8d60d217eed4e6338c8bc
OSiG43 <simon.galand@beyond-the-sea.fr>
2024-05-07 17:33:15 +0100

    Add dashboard help modal

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,19 +1,42 @@
-import {Center, Group, Paper, Stack, Title} from "@mantine/core";
+import {Center, Group, Paper, Stack, Title, ActionIcon, Button} from "@mantine/core";
 import BackTodayTable from "../../components/dashboard/BackTodayTable";
 import {useGetIdentity} from "@refinedev/core";
 import LateReturnTable from "../../components/dashboard/LateReturnTable";
 import OnGoingContractTable from "../../components/dashboard/OnGoingContractTable";
 import WaitingForPaymentTable from "../../components/dashboard/WaitingForPaymentTable";
+import {IconInfoCircle} from "@tabler/icons-react";
+import React from "react";
+import {closeAllModals, closeModal, openModal} from "@mantine/modals";
 
 
 const Dashboard = () => {
 
     const {data: myIdentity} = useGetIdentity();
 
+    function info() {
+        openModal({
+            title: "Tableau de bord",
+            children: (<div>
+                    <p>
+                        Le tableau de bord vous permet de consulter les informations importantes concernant les contrats dont vous êtes le référent.<br/>
+                        Pour obtenir plus de détails sur un contrat, cliquez sur la ligne correspondante.
+                    </p>
+
+                    <Button onClick={() => {closeAllModals()}}>Ok</Button>
+                </div>
+            )
+        })
+    }
+
     return (
         <Stack>
             <Paper>
-                <Center><Title>Tableau de bord</Title></Center>
+                <Center>
+                    <Title>Tableau de bord</Title>
+                    <ActionIcon title="Tableau de bord" color="blue" radius="xl" style={{margin: "0.5em"}} onClick={info}>
+                        <IconInfoCircle/>
+                    </ActionIcon>
+                </Center>
             </Paper>
 
             <Group position="apart" grow style={{alignItems: "stretch"}}>
