ecb008c694b4b56b973686e933153825c30cc136
OSiG43 <simon.galand@beyond-the-sea.fr>
2024-05-09 00:14:24 +0100

    Reset email action in admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.sites.shortcuts import get_current_site
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
@@ -7,6 +8,9 @@ from .models import User
 
 # Register your models here.
 class MyUserAdmin(UserAdmin):
+
+    actions = UserAdmin.actions + ('reset_password',)
+
     # Add the new field to the admin form in the _('Personal info') section
     # Detach the actual fields and add them to the new field
     fieldsets = []
@@ -16,5 +20,28 @@ class MyUserAdmin(UserAdmin):
             fieldsets.append((_('Personal info'), {'fields': current_fields + ('phone',)}))
         else:
             fieldsets.append(fieldset)
+
+    @admin.action(description='Envoi mail reset mdp')
+    def reset_password(self, request, queryset):
+        successCount = 0
+        errors = []
+        for user in queryset:
+            current_site = get_current_site(request)
+            site_name = current_site.name
+            domain = current_site.domain
+
+            success = user.send_reset_password_email(domain, site_name, request.is_secure())
+            if success is True:
+                successCount += 1
+            else:
+                errors.append(success) # if success is not True, it's an error message
+
+        if successCount>0:
+            self.message_user(request, str(successCount) + " mails envoyés avec succès")
+        if errors:
+            #Convert errors to text and join errors with \n
+            errors_string = [str(error) for error in errors]
+            error_text = ",".join(errors_string)
+            self.message_user(request, "Erreurs: " + error_text, level='ERROR')
 
 admin.site.register(User, MyUserAdmin)
--- backend/core/models.py
+++ backend/core/models.py
@@ -1,5 +1,13 @@
+from smtplib import SMTPException
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMultiAlternatives, BadHeaderError
 from django.db import models
+from django.template import loader
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
@@ -13,5 +21,51 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['phone', "first_name", "last_name", "username"]
     USERNAME_FIELD = "email"
 
+    def send_reset_password_email(self, domain, site_name, use_https):
+        user_email = self.email
+        context = {
+            "email": user_email,
+            "domain": domain,
+            "site_name": site_name,
+            "uid": urlsafe_base64_encode(force_bytes(self.pk)),
+            "user": self,
+            "token": default_token_generator.make_token(self),
+            "protocol": "https" if use_https else "http",
+            "timeout": settings.PASSWORD_RESET_TIMEOUT/3600,
+        }
+        return self.send_mail(
+            "Plaforme mobilité - Reset de votre mot de passe",
+            "password_reset_email.html",
+            context,
+        )
+
+    def send_mail(
+        self,
+        subject,
+        email_template_name,
+        context,
+        from_email=None,
+        html_email_template_name=None,
+    ):
+        """
+        Send a django.core.mail.EmailMultiAlternatives to `to_email`.
+        """
+        # Email subject *must not* contain newlines
+        subject = "".join(subject.splitlines())
+        body = loader.render_to_string(email_template_name, context)
+
+        email_message = EmailMultiAlternatives(subject, body, from_email, [self.email])
+        if html_email_template_name is not None:
+            html_email = loader.render_to_string(html_email_template_name, context)
+            email_message.attach_alternative(html_email, "text/html")
+        try:
+            email_message.send()
+            return True
+        except BadHeaderError:
+            return "Entête invalide."
+        except SMTPException as e:
+            return e
+        except Exception as e:
+            return e
 
 
