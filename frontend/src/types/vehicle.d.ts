d8eee46706bbfb5a65f3a75249443f2421e3b3a4
OSiG43 <simon.galand@beyond-the-sea.fr>
2024-05-17 00:58:32 +0100

    correct vehicleWritableField type

--- frontend/src/types/vehicle.d.ts
+++ frontend/src/types/vehicle.d.ts
@@ -2,7 +2,7 @@ import {components} from "./schema";
 
 type Vehicle = components['schemas']['Vehicle'];
 
-type VehicleWritableFields = Omit<Vehicle, "id" | "status">
+type VehicleWritableFields = Omit<Vehicle, "id" | "status" | "parking" | "created_at">
 
 type VehicleTransformedFields = VehicleWritableFields & {photo: string | undefined}
 
