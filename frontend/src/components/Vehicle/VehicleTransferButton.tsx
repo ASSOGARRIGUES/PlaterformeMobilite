247228238edbdbccea5f8522e29cd22957809c44
OSiG43 <simon.galand@beyond-the-sea.fr>
2024-09-22 00:01:03 +0200

    fix: transfer button is now hidded if user has access to less than one action.

--- frontend/src/components/Vehicle/VehicleTransferButton.tsx
+++ frontend/src/components/Vehicle/VehicleTransferButton.tsx
@@ -3,12 +3,16 @@ import {Vehicle} from "../../types/vehicle";
 import {Button} from "@mantine/core";
 import {VehicleStatusEnum} from "../../types/schema.d";
 import CanAccess from "../CanAccess";
+import {useUserActions} from "../../context/UserActionsProvider";
 
 const VehicleTransferButton = ({vehicle} : {vehicle: Vehicle | undefined}) => {
 
     const go = useGo();
 
+    const userActions = useUserActions();
+
     if (!vehicle) return null;
+    if (userActions.actions.length < 2) return null; // if the user have access to less than 2 actions, don't show the transfer option
 
     return (
         <CanAccess permKey={"api.can_transfer_vehicle"}>
